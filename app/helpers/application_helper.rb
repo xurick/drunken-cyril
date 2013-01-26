f36d7c50618f98ee429470c39d3a501a1da85c3b
budwhite <budwhite@gmail.com>
2013-01-26 10:54:30 -0800

    adds ga take #3 .. sigh, need html_safe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,13 @@
 module ApplicationHelper
 
   #https://gist.github.com/818065
-  def google_analytics_js
+  #https://github.com/rails/rails/issues/7320 needs to call html_escape to prevent
+  # Rails3 mandatory escaping 
+  def google_analytics_js(id = null)
 
     content_tag(:script, :type => 'text/javascript') do
       "var _gaq = _gaq || [];
-      _gaq.push(['_setAccount', 'UA-37983247-1']);
+      _gaq.push(['_setAccount', '#{id}']);
       _gaq.push(['_setDomainName', 'mbilify.com']);
       _gaq.push(['_trackPageview']);
 
@@ -13,8 +15,8 @@ module ApplicationHelper
         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();"
-    end if Rails.env.production?
+      })();".html_safe
+    end if !id.blank? && Rails.env.production?
 
   end
 end
