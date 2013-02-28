c196da4c4f8b158561050bd6ca58ea6651e915dd
budwhite <budwhite@gmail.com>
2013-02-28 10:55:11 -0800

    adds ga event tracking to tap_to_call

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,7 +6,8 @@ module ApplicationHelper
   def google_analytics_js(id = null)
 
     content_tag(:script, :type => 'text/javascript') do
-      "var _gaq = _gaq || [];
+      "
+      var _gaq = _gaq || [];
       _gaq.push(['_setAccount', '#{id}']);
       _gaq.push(['_setDomainName', 'mbilify.com']);
       _gaq.push(['_trackPageview']);
@@ -15,7 +16,14 @@ module ApplicationHelper
         var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
         ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
         var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-      })();".html_safe
+      })();
+
+      function trackTapCall() {
+        document.getElementById('tap_to_call').onclick = function() {
+          _gaq.push(['_trackEvent', 'ActionButtons', 'Tap', 'Tap to call']);
+        }
+      }
+      ".html_safe
     end if !id.blank? && Rails.env.production?
 
   end
